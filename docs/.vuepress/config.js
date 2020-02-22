18fb1a43fc1c5e8799c06294348f702e8fd0d55b
xiedong <xiedong@rmitec.cn>
2020-02-22 22:27:27 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,22 +1,26 @@
 module.exports = {
-  base: '/blog-demo/',
-  title: 'blog-demo',
-  description: 'Vuepress blog demo',
+  base: '/',
+  title: '沫沫儿的技能树',
+  description: '沫沫儿的技能树',
   head: [
     ['link', { rel: 'icon', href: '/vue-logo.png' }]
   ],
   themeConfig: {
-  	// 你的GitHub仓库
-    repo: 'https://github.com/Simple-Coder/blog-demo',
-    // 自定义仓库链接文字。
-    repoLabel: 'My GitHub',
   	nav: [
-  		{ text: 'Home', link: '/' },
-  		{ text: 'FirstBlog', link: '/blog/FirstBlog.md' }
+  		{ text: 'FirstBlog', link: '/blog/FirstBlog.md' },
+		{ text: '首页', link: '/' },
+            { text: '高级软件工程师养成攻略', link: '/FAQ/Wiki' },
+            { text: '大数据开发工程实习攻略', link: '/FAQ/Console/A000' },
+            //{ text: 'Store', link: '/Store/' },
+            //{ text: 'Thought', link: '/Thought/' },
+            {
+                text: '关于我',
+                items: [
+                    { text: 'Github', link: 'https://github.com/Simple-Coder' },
+                    { text: '博客园', link: 'https://www.cnblogs.com/rmxd/' }
+                ]
+            }
   	],
-  	sidebar: [
-      ['/', '首页'],
-      ['/blog/FirstBlog.md', '我的第一篇博客']
-    ]
+  	sidebar: false
   }
 }
